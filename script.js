bdc9039c8ff5dd26493d24d2ac5dd7174231943c
pandeynilay62-wq <pandeynilay62@gmail.com>
2026-03-13 10:41:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,86 @@
-let total = 0;
+let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+
+let ctx = document.getElementById("myChart").getContext("2d");
+
+let chart = new Chart(ctx,{
+type:"pie",
+data:{
+labels:["Food","Transport","Shopping","Other"],
+datasets:[{
+data:[0,0,0,0],
+backgroundColor:["#ff6384","#36a2eb","#4bc0c0","#ffcd56"]
+}]
+}
+});
 
 function addExpense(){
 
-let name = document.getElementById("name").value;
-let amount = parseInt(document.getElementById("amount").value);
+let name=document.getElementById("name").value;
+let amount=parseInt(document.getElementById("amount").value);
+let category=document.getElementById("category").value;
 
-let list = document.getElementById("list");
+if(name=="" || amount=="") return;
 
-let li = document.createElement("li");
+let expense={name,amount,category};
 
-li.innerHTML = name + " - ₹" + amount;
+expenses.push(expense);
+
+localStorage.setItem("expenses",JSON.stringify(expenses));
+
+displayExpenses();
+}
+
+function deleteExpense(index){
+
+expenses.splice(index,1);
+
+localStorage.setItem("expenses",JSON.stringify(expenses));
+
+displayExpenses();
+}
+
+function displayExpenses(){
+
+let list=document.getElementById("list");
+
+list.innerHTML="";
+
+let total=0;
+
+let categoryTotals={
+Food:0,
+Transport:0,
+Shopping:0,
+Other:0
+};
+
+expenses.forEach((exp,i)=>{
+
+let li=document.createElement("li");
+
+li.innerHTML=`
+${exp.name} - ₹${exp.amount} (${exp.category})
+<button class="delete-btn" onclick="deleteExpense(${i})">X</button>
+`;
 
 list.appendChild(li);
 
-total = total + amount;
+total+=exp.amount;
 
-document.getElementById("total").innerText = total;
+categoryTotals[exp.category]+=exp.amount;
 
+});
+
+document.getElementById("total").innerText=total;
+
+chart.data.datasets[0].data=[
+categoryTotals.Food,
+categoryTotals.Transport,
+categoryTotals.Shopping,
+categoryTotals.Other
+];
+
+chart.update();
 }
+
+displayExpenses();
